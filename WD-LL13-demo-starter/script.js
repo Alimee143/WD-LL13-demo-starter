b5092c33d15032a0a4f505dd810fb22b41b636da
Zabihullah Alimee <117387745+Alimee143@users.noreply.github.com>
2025-06-30 23:57:00 +0000

    first copy

--- WD-LL13-demo-starter/script.js
+++ WD-LL13-demo-starter/script.js
@@ -1,4 +1,21 @@
 // We'll use fetch() to get a cat fact from the API
 // and display it inside the #cat-fact element
 
+
+fetch('https://catfact.ninja/fact')
+  .then(response => response.json())
+  .then(data => {
+    console.log(data);
+    const catFactEl = document.getElementById('cat-fact');
+    catFactEl.textContent = data.fact;
+
+    //Add styling class
+    catFactEl.classList.add('pretty-fact')
+
+  })
+  .catch(error => {
+    console.error('Somthing went wrong.', error);
+    document.getElementById('cat-fact').textContent = "Failed to load the cat fact!"
+  })
+
 // Once that works, we can apply the "pretty-fact" CSS class too
